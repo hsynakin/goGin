baae10939a86cdc5f8935bc5a12ccbc140cf6973
hsynakin <45360362+hsynakin@users.noreply.github.com>
2018-11-28 17:36:00 +0300

    test

--- ex1/main.go
+++ ex1/main.go
@@ -8,8 +8,12 @@ import (
 
 func main() {
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/**/*html")
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello from %v", "gin")
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+	r.GET("/login", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.html", nil)
 	})
 	r.Run(":8000")
 }
